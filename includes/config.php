5820a6200e531db13fb5c3e91a6267ff3e364681
FABIAN JACOB <phabian.jacob@gmail.com>
2021-07-16 15:04:19 +0300

    My first project

---
+++ includes/config.php
@@ -0,0 +1,28 @@
+<?php
+
+    //database configuration
+    
+    $host       = "ls-792b9344ff5d9c02697c01268ccb68488c23d854.cbdgjqmgsybg.us-west-2.rds.amazonaws.com";
+    $user       = "dbmasteruser";
+    $pass       = "ebook_2021";
+    $database   = "android_ebook_app";
+   
+
+/*
+    $host       = "localhost";
+    $user       = "root";
+    $pass       = "";
+    $database   = "ebook";
+     */
+
+    $connect = new mysqli($host, $user, $pass, $database);
+
+    if (!$connect) {
+        die ("connection failed: " . mysqli_connect_error());
+    } else {
+        $connect->set_charset('utf8');
+    }
+
+    $ENABLE_RTL_MODE = 'false';
+
+?>
